21fb94fafcafb9cf5504a27c1acf263f5f14fbd3
amit ben basat <amitbasat124@gmail.com>
2022-09-20 15:16:08 +0300

    ading the function that get all the player by year and by teamname in the server

---
+++ backend/server.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi import Response
+import requests
+from tems_id import teams_id
+app = FastAPI()
+league = "league"
+standard="standard"
+teams="teams"
+teamId="teamId"
+
+
+@app.get('/allThePlayersInSpesificYearAndTeam/')
+async def get_all_the_players(year,teamname):
+    tems_id_by_name = teams_id[teamname]
+    res = requests.get(f'http://data.nba.net/10s/prod/v1/{year}/players.json')
+    all_the_player_in_year=res.json()[league][standard]
+    player_in_the_teams_id = list(filter(lambda player:player[teamId]==tems_id_by_name,all_the_player_in_year))
+    return player_in_the_teams_id
+
+
+
+
+
+
+if __name__ == "__main__":
+     uvicorn.run("server:app", host="0.0.0.0", port=8001,reload=True)
+
